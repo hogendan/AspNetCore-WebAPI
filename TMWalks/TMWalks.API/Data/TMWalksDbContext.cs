ff304e938aac3f7b1cab24c0d17405f317baeb62
渡邉友樹 <>
2023-11-19 20:24:03 +0900

    feat: Add DbContext class

---
+++ TMWalks/TMWalks.API/Data/TMWalksDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TMWalks.API;
+
+public class TMWalksDbContext : DbContext
+{
+    public TMWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+    {
+        
+    }
+
+    public DbSet<Difficulty> Difficulties { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Walk> Walks { get; set; }
+}
