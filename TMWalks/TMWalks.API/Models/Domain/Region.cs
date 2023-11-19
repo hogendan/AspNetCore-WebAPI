e318aec61d0e615807b6fc7945496a923d57091d
渡邉友樹 <hogendan@gmail.com>
2023-11-19 16:47:14 +0900

    feat: コントローラー、ドメイン追加

---
+++ TMWalks/TMWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,9 @@
+﻿namespace TMWalks.API;
+
+public class Region
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? RegionImageUrl { get; set; }
+}
