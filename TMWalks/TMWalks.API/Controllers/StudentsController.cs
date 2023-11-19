a3f3059ca0e5f0f4b8e05f974ae7a97553e18000
渡邉友樹 <>
2023-11-19 16:47:14 +0900

    feat: コントローラー、ドメイン追加

---
+++ TMWalks/TMWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TMWalks.API;
+
+// https://localhost:portnumber/api/students
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+    // GET: https://locahost:portnumber/api/studentes
+    [HttpGet]
+    public IActionResult GetAllStudents() {
+        string[] students = new string[] { "John", "Jane", "Mark", "Emily", "David" };
+
+        return Ok(students);
+    }
+
+}
---
+++ TMWalks/TMWalks.API/Models/Domain/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace TMWalks.API;
+
+public class Difficulty
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+}
