6bb4e887d2dee4f19cc48b4dede25842afe798b8
渡邉友樹 <hogendan@gmail.com>
2023-11-26 19:29:06 +0900

    fix: DbContext の DI 設定を追加

--- TMWalks/TMWalks.API/Program.cs
+++ TMWalks/TMWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TMWalks.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TMWalksDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TMWalksConnectionString"))
+);
 
 var app = builder.Build();
 
