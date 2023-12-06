c4ecec48ed85845e71cd1c61bc169a34ab78ee64
渡邉友樹 <hogendan@gmail.com>
2023-12-07 08:12:18 +0900

    fix: Domain Model To DTO

---
+++ TMWalks/TMWalks.API/Models/DTO/RegionDto.cs
@@ -0,0 +1,9 @@
+﻿ namespace TMWalks.API;
+
+public class RegionDto
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? RegionImageUrl { get; set; }
+}
