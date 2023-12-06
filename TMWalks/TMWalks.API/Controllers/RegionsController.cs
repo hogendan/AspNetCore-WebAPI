97d138c2a187c2659cf36444b0f0c5f77e37cd66
渡邉友樹 <>
2023-12-07 08:12:18 +0900

    fix: Domain Model To DTO

--- TMWalks/TMWalks.API/Controllers/RegionsController.cs
+++ TMWalks/TMWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Sockets;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TMWalks.API;
 
@@ -17,9 +18,22 @@ public class RegionsController : ControllerBase
     // GET: https://localhost:portnumber/api/regions
     [HttpGet]
     public IActionResult GetAll() {
-        var regions = dbContext.Regions.ToList();
+        // Get Data From Database - Domain models
+        var regionsDomain = dbContext.Regions.ToList();
         
-        return Ok(regions);
+        // Map Domain Models to DTOs
+         var regionsDto = new List<RegionDto>();
+         foreach(var regionDomain in regionsDomain) {
+            regionsDto.Add(new RegionDto {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl,
+            });
+         }
+
+        // Return DTOs
+        return Ok(regionsDto);
     }
 
     // GET SINGLE REGION (Get Region By ID)
@@ -30,12 +44,48 @@ public class RegionsController : ControllerBase
         // Find は Primary Key を探す
         // var region = dbContext.Regions.Find(id);
 
-        var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+        // Get Region Domain Model From Database
+        var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
 
-        if (region == null) {
+        if (regionDomain == null) {
             return NotFound();
         }
 
-        return Ok(region);
+        // Map/Converter Region Domain Model to Region DTO
+        var regionDto = new RegionDto {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl,
+        };
+
+        // Return DTO back to client
+        return Ok(regionDto);
+    }
+
+    // POST To Create New Region
+    // POST: https://localhost:portnumber/api/regions
+    [HttpPost]
+    public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto) {
+        // Map or Convert DTO to Domain Model
+        var regionDomainModel = new Region {
+            Code = addRegionRequestDto.Code,
+            Name = addRegionRequestDto.Name,
+            RegionImageUrl = addRegionRequestDto.RegionImageUrl,
+        };
+
+        // Use Domain Model to create Region
+        dbContext.Regions.Add(regionDomainModel);
+        dbContext.SaveChanges();
+
+        // Map Domain model back to DTO
+        var regionDto = new RegionDto {
+            Id = regionDomainModel.Id,
+            Code = regionDomainModel.Code,
+            Name = regionDomainModel.Name,
+            RegionImageUrl = regionDomainModel.RegionImageUrl,
+        };
+
+        return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
     }
 }
