cfd1817b46a767f3f31066ca432d2ea78a084a8f
渡邉友樹 <hogendan@gmail.com>
2023-12-08 13:42:32 +0900

    fix: Update, Delete アクションメソッド

---
+++ TMWalks/TMWalks.API/Models/DTO/UpdateRetionRequesstDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TMWalks.API;
+
+public class UpdateRetionRequesstDto
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? RegionImageUrl { get; set; }
+}
