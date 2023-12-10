38aebfff8d8c10b8510840c031fb3b29fa7e6a8b
渡邉友樹 <>
2023-12-10 09:24:12 +0900

    feat: Repository クラス追加

---
+++ TMWalks/TMWalks.API/Repositories/InMemoryRegionRepository.cs
@@ -0,0 +1,37 @@
+﻿using TMWalks.API.Repositories;
+
+namespace TMWalks.API;
+
+public class InMemoryRegionRepository : IRegionRepository
+{
+    public Task<Region> CreateAsync(Region region)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Region?> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Region>> GetAllAsync()
+    {
+        return new List<Region> {
+            new Region {
+                Id = Guid.NewGuid(),
+                Code = "SAM",
+                Name = "Sameer's Region Name"
+            }
+        };
+    }
+
+    public Task<Region?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Region?> UpdateAsync(Guid id, Region region)
+    {
+        throw new NotImplementedException();
+    }
+}
