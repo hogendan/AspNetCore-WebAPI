ee30f527eda98b24d71105397037ea5e547dfc26
渡邉友樹 <>
2024-01-09 21:09:15 +0900

    fix: Authentication 対応
    - Jwt ライブラリパッケージ追加
    - Auth用DbContextクラス作成
    - Jwt設定追加
    - などなど

---
+++ TMWalks/TMWalks.API/Data/TMWalksAuthDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TMWalks.API;
+
+public class TMWalksAuthDbContext : IdentityDbContext
+{
+    public TMWalksAuthDbContext(DbContextOptions<TMWalksAuthDbContext> options) : base(options)
+    {
+    }
+}
