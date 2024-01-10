c7c645b6c19525e90f270c8f2bc3202b03793d2f
渡邉友樹 <hogendan@gmail.com>
2024-01-10 20:00:47 +0900

    feat: JWT Token の生成

---
+++ TMWalks/TMWalks.API/Repositories/TokeRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TMWalks.API;
+
+public class TokeRepository : ITokenRepository
+{
+    private readonly IConfiguration configuration;
+
+    public TokeRepository(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+    public string CreateJWTToken(IdentityUser user, List<string> roles)
+    {
+        // Create claims (Token を作るために必要)
+        var claims = new List<Claim>();
+
+        claims.Add(new Claim(ClaimTypes.Email, user.Email));
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            configuration["Jwt:Issuer"],
+            configuration["Jwt:Audience"],
+            claims,
+            expires: DateTime.Now.AddMinutes(15),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
