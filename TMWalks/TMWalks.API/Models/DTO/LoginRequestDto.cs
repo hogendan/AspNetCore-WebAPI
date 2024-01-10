3e9d1494dad92e26c195802c5c538bcbd3651bea
渡邉友樹 <>
2024-01-10 16:03:54 +0900

    feat: ログインAction追加

---
+++ TMWalks/TMWalks.API/Models/DTO/LoginRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TMWalks.API;
+
+public class LoginRequestDto
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Username { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
