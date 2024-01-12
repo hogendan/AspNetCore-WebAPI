7aa9822b7d0c3280ab0636f66cd59a62010d9650
渡邉友樹 <hogendan@gmail.com>
2024-01-12 20:06:35 +0900

    feat: VersioningとVersioningのSwagger対応

---
+++ TMWalks/TMWalks.API/Controllers/CountryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TMWalks.API;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+[ApiVersion("2.0")]
+[ApiController]
+public class CountryController : ControllerBase
+{
+    [MapToApiVersion("1.0")]
+    [HttpGet]
+    public IActionResult GetV1() 
+    {
+        var dto = new CountryDtoV1 {
+            Id = 1,
+            Name = "Japan v1",
+        };
+
+        return Ok(dto);
+    }
+
+    [MapToApiVersion("2.0")]
+    [HttpGet]
+    public IActionResult GetV2() 
+    {
+        var dto = new CountryDtoV2 {
+            Id = 1,
+            CountryName = "Japan v2",
+        };
+
+        return Ok(dto);
+    }
+}
---
+++ TMWalks/TMWalks.API/Models/DTO/CountryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TMWalks.API;
+
+public class CountryDtoV1
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class CountryDtoV2
+{
+    public int Id { get; set; }
+    public string CountryName { get; set; }
+}
