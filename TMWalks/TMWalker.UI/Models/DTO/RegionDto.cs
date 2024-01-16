6fd10ea5fe1c93eaa7b14c49259e3a17311e679b
渡邉友樹 <>
2024-01-16 10:48:56 +0900

    feat: WebAPI を呼び出す UI プロジェクト追加

---
+++ TMWalks/TMWalker.UI/Models/DTO/RegionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TMWalker.UI;
+
+public class RegionDto
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? RegionImageUrl { get; set; }
+}
